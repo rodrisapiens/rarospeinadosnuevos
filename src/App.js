84626ab1b22f86540128f83b706a3c27bfe97738
rodrisapiens <rniveyroreguero@frba.utn.edu.ar>
2022-03-05 13:46:58 -0300

    mockup done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import "./styles/app.css"
+import vynil from "./images/favpng_phonograph-record-lp-record.png"
+import music from './audio/Esos raros peinados nuevos - Charly Garcia (Piano Bar).mp3'
 function App() {
   const [response, setResponse] = useState("WE PLAY MUSIC")
   const [showButton, setShowButton] = useState(false);
@@ -10,15 +12,23 @@ function App() {
       }, 8000)
 
     }
-    if(response==="ROCK")
-    {
+    if (response === "ROCK") {
       setTimeout(() => {
         setShowButton(true);
-      },4000)
+      }, 4500)
+    }
+    if (response === "lets see") {
+      setTimeout(() => {
+        setResponse("play music")
+      }, 3000)
     }
 
   }
     , [response])
+  function handleMusic() {
+    let audio = new Audio(music);
+    audio.play();
+  }
 
   return (
     <div className="App">
@@ -29,15 +39,31 @@ function App() {
             <div className="word">are</div>
             <div className="word">you </div>
             <div className="word">doing?</div>
-            {showButton ? <button className="response" onClick={() => { setResponse("ROCK");setShowButton(false) }}>{response}</button> : null}
+            {showButton ? <button className="response" onClick={() => { setResponse("ROCK"); setShowButton(false) }}>{response}</button> : null}
           </div>
           : response === "ROCK" ?
             <div className="morphing">
               <div className="secondWords">WHAT </div>
               <div className="secondWords">MUSIC?</div>
-              {showButton ? <button className="response" onClick={() => { setResponse("WE PLAY MUSIC"); setShowButton(false); }}>{response}</button> : null}
+              {showButton ? <button className="response" onClick={() => { setResponse("lets see"); setShowButton(false); }}>{response}</button> : null}
             </div>
-            : null
+            : response === "lets see" ?
+              <div className="morphing">
+                <div className="thirdWords">LETS SEE...</div>
+                <div className="thirdWords">LETS SEE...</div>
+                <div className="thirdWords">LETS SEE...</div>
+              </div>
+              : response === "play music" ?
+                <div className="play">
+                  <h4 className="jp">RAROS PEINADOS NUEVOS - CRAZY NEW HAIRSTYLES</h4>
+                  <h5>by Juan Pedro Ramos </h5>
+                  <img src={vynil} alt="" className="vynil"></img>
+                  {handleMusic()}
+                </div>
+
+                : null
+
+
       }
     </div>
   );
