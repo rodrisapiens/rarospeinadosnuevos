0943e6cb30080d05d71a9477939b07ed7c2dd09f
rodrisapiens <rniveyroreguero@frba.utn.edu.ar>
2022-03-14 02:28:27 -0300

    contacts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./styles/app.css"
+import {ReactComponent as Instagram} from './images/Instagram.svg'
 import historia from './audio/001_HISTORIA.wav'
 import music1 from './audio/002_CLOSE TO THE EDGE(1).wav'
 import music2 from './audio/003_RUTA PERDEDORA(1).wav'
@@ -13,6 +14,9 @@ import { ReactComponent as BackGroundD } from "./images/desk/h_fondo.svg"
 import { ReactComponent as PuaD } from "./images/desk/h_pua.svg"
 import { ReactComponent as BrilloD } from "./images/desk/h_brillo.svg"
 import { ReactComponent as ViniloD } from "./images/desk/h_vinilo.svg"
+const ailin ="https://www.instagram.com/ailinestrella/";
+const jupe="https://www.instagram.com/jupe.arrt/";
+const dante="https://www.instagram.com/dantefisi/";
 function App() {
   const [response, setResponse] = useState("WE PLAY MUSIC")
   const [showButton, setShowButton] = useState(false);
@@ -104,7 +108,16 @@ function App() {
         break;
     }
   }
-
+function HandleContact(e)
+{
+  const target=e.currentTarget.textContent;
+  if(target==="Ailin Estrella")
+  window.open(ailin);
+  if(target==="Juan Pedro Ramos")
+  window.open(jupe);
+  if(target==="Dante Fisi")
+  window.open(dante);
+}
   return (
     <div className="App">
       {
@@ -125,11 +138,18 @@ function App() {
               </div>
               : response === "presenting" ?
                 <div className="present">
-                  {window.innerWidth<500?<Pua className="pua" />:<PuaD className="pua"/>}
-                  {window.innerWidth<500?<Vinilo className="vinilo" />:<ViniloD className="vinilo"/>}
-                  {window.innerWidth<500?<Brillo className="brillo" />:<BrilloD className="brillo"/>}
-                  {window.innerWidth<500?<BackGround className="backGround"/>:<BackGroundD className="backGround"/>}
+                  {window.innerWidth<600?<Pua className="pua" />:<PuaD className="pua"/>}
+                  {window.innerWidth<600?<Vinilo className="vinilo" />:<ViniloD className="vinilo"/>}
+                  {window.innerWidth<600?<Brillo className="brillo" />:<BrilloD className="brillo"/>}
+                  {window.innerWidth<600?<BackGround className="backGround"/>:<BackGroundD className="backGround"/>}
                   <h1 className="trackDescriber">Now playing: {music}</h1>
+                  <div className="contacts">
+                    <h1 className="contactTitle">Contacts</h1>
+                    <Instagram className="instagram"/>
+                    <button className="contact" onClick={HandleContact}>Ailin Estrella</button>
+                    <button className="contact" id="jupe" onClick={HandleContact}>Juan Pedro Ramos</button>
+                    <button className="contact" onClick={HandleContact}>Dante Fisi</button>
+                  </div>
                   {handleMusic(music)}
                 </div>:null
 
