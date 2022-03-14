ddd701fb3cdf2a528240ea05e44a73f6e1dd9b58
rodrisapiens <rniveyroreguero@frba.utn.edu.ar>
2022-03-13 23:19:31 -0300

    had to undo 6 commits because of large files

--- src/App.js
+++ src/App.js
@@ -1,33 +1,105 @@
 import { useEffect, useState } from "react";
 import "./styles/app.css"
 import vynil from "./images/favpng_phonograph-record-lp-record.png"
-import music from './audio/Esos raros peinados nuevos - Charly Garcia (Piano Bar).mp3'
+import historia from './audio/001_HISTORIA.wav'
+import music1 from './audio/002_CLOSE TO THE EDGE(1).wav'
+import music2 from './audio/003_RUTA PERDEDORA(1).wav'
+import music3 from './audio/004_SUPPERSREADY(1).wav'
+import music4 from './audio/005_THE COURT OF THE KRIMSON KING(1).wav'
+import { ReactComponent as Pua } from './images/mobile/puaD.svg'
+import { ReactComponent as Vinilo } from './images/mobile/ViniloD.svg'
+import { ReactComponent as BackGround } from "./images/mobile/FondoD.svg"
+import { ReactComponent as Brillo } from "./images/mobile/BrilloD.svg"
 function App() {
   const [response, setResponse] = useState("WE PLAY MUSIC")
   const [showButton, setShowButton] = useState(false);
+  const [music,setMusic]=useState("historia");
   useEffect(() => {
     if (response === "WE PLAY MUSIC") {
       setTimeout(() => {
         setShowButton(true);
-      }, 8000)
+      }, 2500)
 
     }
     if (response === "ROCK") {
       setTimeout(() => {
         setShowButton(true);
-      }, 4500)
+      }, 2500)
     }
     if (response === "lets see") {
       setTimeout(() => {
-        setResponse("play music")
+        setResponse("presenting")
+      }, 3000)
+    }
+    if (response === "band photo") {
+      setTimeout(() => {
+        setResponse("presenting")
+      }, 3000)
+    }
+    if (response === "presenting") {
+      setTimeout(() => {
       }, 3000)
     }
 
   }
     , [response])
-  function handleMusic() {
-    let audio = new Audio(music);
-    audio.play();
+  function handleMusic(select) {
+    let audio;
+    switch (select) {
+      case "historia":
+        audio = new Audio(historia);
+        setTimeout(()=>{
+          audio.play();
+        },2700)
+        audio.onended= function(){
+          handleMusic("music1");
+          console.log("termine hisotria")
+          setMusic("CLOSE TO THE EDGE");
+        }
+        break;
+
+      case "music1":
+        audio = new Audio(music1);
+        audio.play();
+        audio.onended= function(){
+          handleMusic("music2");
+          console.log("termine music1")
+          setMusic("RUTA PERDEDORA");
+        }
+
+        break;
+
+      case "music2":
+        audio = new Audio(music2);
+        audio.play();
+        audio.onended= function(){
+          handleMusic("music3");
+          setMusic("SUPPERSREADY");
+
+        }
+        break;
+
+      case "music3":
+        audio = new Audio(music3);
+        audio.play();
+        audio.onended= function(){
+          handleMusic("music4");
+          setMusic("THE COURT OF THE KRIMSON KING");
+        }
+        break;
+
+      case "music4":
+        audio = new Audio(music4);
+        audio.play();
+        audio.onended= function(){
+          setResponse("WE PLAY MUSIC");
+        }
+        break;
+
+      default:
+
+        break;
+    }
   }
 
   return (
@@ -35,33 +107,38 @@ function App() {
       {
         response === "WE PLAY MUSIC" ?
           <div className="morphing">
-            <div className="word">what </div>
-            <div className="word">are</div>
-            <div className="word">you </div>
-            <div className="word">doing?</div>
+            <div className="word">what are you doing?</div>
             {showButton ? <button className="response" onClick={() => { setResponse("ROCK"); setShowButton(false) }}>{response}</button> : null}
           </div>
           : response === "ROCK" ?
             <div className="morphing">
-              <div className="secondWords">WHAT </div>
-              <div className="secondWords">MUSIC?</div>
+              <div className="wordOne">WHAT MUSIC?</div>
               {showButton ? <button className="response" onClick={() => { setResponse("lets see"); setShowButton(false); }}>{response}</button> : null}
             </div>
             : response === "lets see" ?
               <div className="morphing">
-                <div className="thirdWords">LETS SEE...</div>
-                <div className="thirdWords">LETS SEE...</div>
-                <div className="thirdWords">LETS SEE...</div>
-              </div>
-              : response === "play music" ?
-                <div className="play">
-                  <h4 className="jp">RAROS PEINADOS NUEVOS - CRAZY NEW HAIRSTYLES</h4>
-                  <h5>by Juan Pedro Ramos </h5>
-                  <img src={vynil} alt="" className="vynil"></img>
-                  {handleMusic()}
-                </div>
+                <div className="wordTwo">LETS SEE...</div>
 
-                : null
+              </div>
+              : response === "presenting" ?
+                <div className="present">
+                  <Pua className="pua" />
+                  <Vinilo className="vinilo" />
+                  <Brillo className="brillo" />
+                  <BackGround className="backGround" />
+                  <h1 className="trackDescriber">Now playing: {music}</h1>
+                  {handleMusic(music)}
+                </div>
+                : response === "band photo" ?
+                  <div className="play">
+                    <h4 className="jp">RAROS PEINADOS NUEVOS - CRAZY NEW HAIRSTYLES</h4>
+                    <h5>by Juan Pedro Ramos </h5>
+                    <img src={vynil} alt="" className="vynil"></img>
+                    
+                    {handleMusic("historia")}
+                  </div>
+
+                  : response==="final"?<h1>el final xd</h1>:null
 
 
       }
